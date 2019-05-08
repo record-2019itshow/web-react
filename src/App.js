1c901191e884d5468f06922db70fefe8fba9bad1
Yoonsu Kang <kangys0725@naver.com>
2019-05-08 21:46:36 +0900

    header 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
+import Main from './main';
 import './App.css'
 
 class App extends Component{
   render(){
     return(
-		<div>
-        <h1 class="App-title">RECORD</h1>
-    </div>
+		<Main/>
     );
   }
 }
